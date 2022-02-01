7298d5ee732834570e1223c631036b7338f3aaa5
mohamedkhaledegy <74407534+mohamedkhaledegy@users.noreply.github.com>
2022-02-01 07:29:54 +0200

    mm

--- aman/views.py
+++ aman/views.py
@@ -1,6 +1,8 @@
 from .models import Store
 from django.shortcuts import render
-
+from django.core.paginator import Paginator
+from django.db.models import Count
+from django.db.models import Q
 # Create your views here.
 
 
@@ -17,3 +19,31 @@ def store_list(request):
         'countstores':store_count,
             }
     return render(request,'store_list.html',context)
+
+
+# def productlist(request , category_slug=None):
+#     category = None
+#     productlist = Product.objects.all()
+#     categorylist = Category.objects.annotate(total_products=Count('product'))
+
+#     if category_slug :
+#         category = get_object_or_404(Category ,slug=category_slug)
+#         productlist = productlist.filter(category=category)
+
+#     search_query = request.GET.get('q')
+#     if search_query :
+#         productlist = productlist.filter(
+#             Q(name__icontains = search_query) |
+#             Q(description__icontains = search_query)|
+#             Q(condition__icontains = search_query)|
+#             Q(brand__brand_name__icontains = search_query) |
+#             Q(category__category_name__icontains = search_query) 
+#         )
+
+#     paginator = Paginator(productlist, 1) # Show 25 contacts per page
+#     page = request.GET.get('page')
+#     productlist = paginator.get_page(page)
+#     template = 'Product/product_list.html'
+
+#     context = {'product_list' : productlist , 'category_list' : categorylist ,'category' : category }
+#     return render(request , template , context)
